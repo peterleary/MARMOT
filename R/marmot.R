1c76a280fcc47ae9cec2b40654ebc5e3e7e08a4f
Peter Leary <peter.leary@me.com>
2025-03-10 16:32:47 +0100

    More fixes to package name stuff

--- R/marmot.R
+++ R/marmot.R
@@ -15,7 +15,11 @@ marmot <- function(metadata = NULL, name = "Title", render = FALSE) {
   fp <- dirname(metadata)
   
   # Read Excel file
-  params_df <- openxlsx::read.xlsx(metadata, sheet = "Pipeline Settings")
+  # TODO: add check to make sure sheet "Pipeline Settings" exists 
+  if(!any(grepl("pipeline settings", openxlsx::getSheetNames("~/Desktop/FGCZ/MARMOT/MARMOT_Metadata.xlsx"), ignore.case = T))) {
+    stop("Oops! The marmots can't find a 'Pipeline Settings' tab in your Excel Metadata file.")
+  }
+  params_df <- read.xlsx(metadata, sheet = "Pipeline Settings")
   
   cantBeBlank <- c(
     "clusteringMethodToUse", "markersToClusterBy", "kValuesIWant", "knn", 
