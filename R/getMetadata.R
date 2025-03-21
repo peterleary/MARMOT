c2f81202b498354aa86e80f37312a4f2d80890a6
Lydia Kirsche <156294553+LPrunus@users.noreply.github.com>
2025-03-21 15:19:06 +0100

    Update getMetadata.R

--- R/getMetadata.R
+++ R/getMetadata.R
@@ -8,6 +8,14 @@
 #' \dontrun{
 #' getMetadata(folder = "~/Desktop/Flow_Data/")
 #' }
+#' getMetadata <- function(folder = NULL) {
+#'  openxlsx::write.xlsx(x = system.file("pipeline/", "MARMOT_Metadata.xlsx", package = "MARMOT"), file = file.path(folder, "MARMOT_Metadata.xlsx"))
+#' }
+
 getMetadata <- function(folder = NULL) {
-  openxlsx::write.xlsx(x = system.file("pipeline/", "MARMOT_Metadata.xlsx", package = "MARMOT"), file = file.path(folder, "MARMOT_Metadata.xlsx"))
+  file_path <- system.file("pipeline", "MARMOT_Metadata.xlsx", package = "MARMOT")
+  if (file_path == "") {
+    stop("MARMOT_Metadata.xlsx not found. Ensure it is placed in 'inst/pipeline/' and the package is rebuilt.")
+  }
+  file.copy(from = file_path, to = file.path(folder, "MARMOT_Metadata.xlsx"), overwrite = TRUE)
 }
