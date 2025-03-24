d900fae811f6957e416d9987f6e03d46610229e9
Lydia Kirsche <156294553+LPrunus@users.noreply.github.com>
2025-03-24 16:53:23 +0100

    Update getMetadata.R

--- R/getMetadata.R
+++ R/getMetadata.R
@@ -12,16 +12,17 @@
 #' openxlsx::write.xlsx(x = system.file("pipeline/", "MARMOT_Metadata.xlsx", package = "MARMOT"), file = file.path(folder, "MARMOT_Metadata.xlsx"))
 #' }
 
-getMetadata <- function() {
-  # Locate the metadata file in the installed package
-  pkg_file <- system.file("pipeline", "MARMOT_Metadata.xlsx", package = "MARMOT")
+getMetadata <- function(destfile) {
+  # Define the path to the metadata file inside the package
+  metadata_path <- system.file("pipeline", "MARMOT_Metadata.xlsx", package = "MARMOT")
   
-  # Check if the file exists and is found
-  if (!nzchar(pkg_file) || !file.exists(pkg_file)) {
-    stop("Metadata file not found in the package installation. Please ensure 'inst/pipeline/MARMOT_Metadata.xlsx' is included in your package.")
+  # Check if the file exists
+  if (metadata_path == "") {
+    stop("Metadata file not found. Ensure the package is installed correctly.")
   }
   
-  # Read and return the metadata from the Excel file
-  metadata <- readxl::read_excel(pkg_file)
-  return(metadata)
+  # Copy the file to the user-defined destination
+  file.copy(metadata_path, destfile)
+  
+  message("Metadata file has been saved to: ", destfile)
 }
