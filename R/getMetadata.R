23231f285bfead1eb58bfcbf0f9de03c418599e5
Peter Leary <peter.leary@me.com>
2025-03-27 09:33:46 +0100

    Fixed getMetadata function

--- R/getMetadata.R
+++ R/getMetadata.R
@@ -1,27 +1,14 @@
-#' Download the MARMOT metadata file
-#'
-#' This function copies the metadata Excel file (`MARMOT_Metadata.xlsx`) from 
-#' the package directory to a user-specified location.
-#'
-#' @param localfilepath A character string specifying the full path where the 
-#' metadata file should be saved. If `NULL` (default), the file will be saved 
-#' in the current working directory as `"MARMOT_Metadata.xlsx"`.
-#' 
-#' @details 
-#' The metadata file is stored within the package's `inst/pipeline/` directory. 
-#' This function retrieves the file path dynamically and allows users to copy it 
-#' to a desired location.
-#'
+#' @title getMetadata
+#' @description This function copies the metadata file from the package to a user-defined location.
+#' @return A message indicating the location of the metadata file.
+#' @author Peter Leary
+#' @export
+#' @import Rcpp
 #' @examples
 #' \dontrun{
-#' # Download to the current working directory
-#' getMetadata()
-#' 
-#' # Download to a specific location
-#' getMetadata("C:/Users/YourName/Documents/MARMOT_Metadata.xlsx")
+#' marmot(metadata = "~/Desktop/Flow_Data/MARMOT_metadata.xlsx", name = "Study Name", render = FALSE)
 #' }
-#' 
-getMetadata <- function(localfilepath) {
+getMetadata <- function(localfilepath = ".") {
   # Define the path to the metadata file inside the package
   metadata_path <- system.file("pipeline", "MARMOT_Metadata.xlsx", package = "MARMOT")
   
