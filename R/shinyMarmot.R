356fea72bee406b81c9f3a317ddfa956129c3bcb
Peter Leary <peter.leary@me.com>
2025-05-06 14:49:53 +0200

    Small fix to shiny marmot function

--- R/shinyMarmot.R
+++ R/shinyMarmot.R
@@ -9,12 +9,12 @@
 #' shniyMarmot(marmot_output = "~/Desktop/Flow_Data/Results_2025-03-10_11_01_01/R_files")
 #' }
 shinyMarmot <- function(marmot_output = NA, demo = NA) {
-  folder <- system.file("app", package = "shinyMarmot")
+  folder <- system.file("app", package = "MARMOT")
   if (!is.na(demo) & is.na(marmot_output)) {
-    marmot_output <- system.file("examples/R_files/", package = "shinyMarmot")
+    marmot_output <- system.file("examples/R_files/", package = "MARMOT")
   }
   if (is.na(demo) & is.na(marmot_output)) {
-    marmot_output <- system.file("examples/R_files/", package = "shinyMarmot")
+    marmot_output <- system.file("examples/R_files/", package = "MARMOT")
   }
   marmot_output <<- tools::file_path_as_absolute(marmot_output)
   shiny::runApp(appDir = folder)
