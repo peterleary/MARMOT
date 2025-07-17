8afe257ce902040c0eb17d2e1c68a9c9bc848ff9
Peter Leary <peter.leary@me.com>
2025-07-17 10:19:24 +0200

    More conda fun

--- conda/install.R
+++ conda/install.R
@@ -1,2 +1,2 @@
-if (!requireNamespace("pak", quietly = TRUE)) install.packages("pak")
+install.packages("pak", repos = "https://cran.rstudio.com", type = "source")
 pak::pkg_install(c("peterleary/MARMOT", "scGate"), ask = F, dependencies = T)
